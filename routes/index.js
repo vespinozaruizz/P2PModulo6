ad8241a9fd077f6d7d80706bda69a07dda082a1a
Victor Hugo Espinoza Ruiz <vespinozaruiz@hotmail.com>
2015-07-23 06:05:22 -0500

    P2PModulo6

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var quizController = require('../controllers/quiz_controller');
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz Victor' });
+});
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+router.get('/quizes/author',quizController.author)
+module.exports = router;
